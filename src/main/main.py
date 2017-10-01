62e3cfb6466a4824b2ccb751bf5804c5a302589f
root <root@instance-1.c.first-project-181415.internal>
2017-10-01 22:12:11 +0000

    Added initial code

---
+++ src/main/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os
+
+class Main :
+    def __init__(self):
+        print("Main obj is created")
+    def start(self):
+        print("Main is started")
+# End of Main Class
+    
+obj = Main()
+obj.start()
+
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-pwd
+
+# Exceute the following code, after creating the VM
 sudo apt-get update
 sudo apt-get install git-core
 git clone https://github.com/balajigan/gcp.git
+python src/main/main.py
 
